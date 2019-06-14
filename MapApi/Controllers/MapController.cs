4854d2f0507525fcdb88274b2bc525ce7c705848
Kashan Arshad <KashanArshad@tfl.gov.uk>
2019-06-14 15:18:33 +0100

    Initial journey coordinator

--- MapApi/Controllers/MapController.cs
+++ MapApi/Controllers/MapController.cs
@@ -6,6 +6,8 @@ using System.Net.Mime;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Xml;
+using MapApi.Models;
+using MapApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MapApi.Controllers
@@ -14,14 +16,39 @@ namespace MapApi.Controllers
     [ApiController]
     public class MapController : ControllerBase
     {
+        private IMarkerRepository _markerRepo;
+        private IJourneyRepository _journeyRepo;
+        private IIntersectionService _interactionService;
+
+
+        public MapController(IMarkerRepository markerRepo, IJourneyRepository journeyRepo, IIntersectionService interactionService)
+        {
+            _markerRepo = markerRepo;
+            _journeyRepo = journeyRepo;
+            _interactionService = interactionService;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<string> Get()
+        public ActionResult<string> Get(int journeyId)
         {
+            var journeyOptions = _journeyRepo.GetRoutesForJourney(journeyId);
+            var pollutionMarkers = _markerRepo.GetMarkers();
+
+            //EnrichedRoute enrichedRoute =
+            foreach (var journeyOption in journeyOptions)
+            {
+                _interactionService.FindMarkersOnRoute(journeyOption.Coordinates, pollutionMarkers);
+            }
+
+            /*
+             4. Create kml object to return kev and assala formats - route and route enriched info & source of enriched info (for overlay)
+            */
+
             var filePath = Path.Combine(Environment.CurrentDirectory, "Test.kml");
             var kml = new XmlDocument();
             kml.Load(filePath);
-
+            
             return kml.OuterXml;
         }
     }
---
+++ MapApi/Models/EnrichedRoute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapApi.Models
+{
+    public class EnrichedRoute
+    {
+        public Route Route { get; set; }
+        public int PollutionScore { get; set; }
+    }
+}
