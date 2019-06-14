ac73c803e90dab0574d27d5aa54a3cbe37c56f9d
Kev Hannen <kev@stadion.io>
2019-06-14 14:54:25 +0100

    display some map directions

--- src/MobileMapHack/App.tsx
+++ src/MobileMapHack/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {MapView} from "expo";
+import MapViewDirections from 'react-native-maps-directions';
+
+const origin = {latitude: 51.4511732, longitude: -0.2138706};
+const destination = {latitude: 51.5250836, longitude: -0.0769465};
+const GOOGLE_MAPS_APIKEY = ' AIzaSyCp1Qch96YhxPn7hzE1zQd65-_12ytnECI ';
 
 export default function App() {
 
@@ -9,12 +14,17 @@ export default function App() {
             <MapView
                 style={{flex: 1}}
                 initialRegion={{
-                    latitude: 51.509865,
+                    latitude: 51.509864,
                     longitude: -0.118092,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
-                }}
-            />
+                }}>
+                <MapViewDirections
+                    origin={origin}
+                    destination={destination}
+                    apikey={GOOGLE_MAPS_APIKEY}
+                    />
+            </MapView>
             <View>
                 <Text style={{flex: 0}}>Stuff</Text>
             </View>
