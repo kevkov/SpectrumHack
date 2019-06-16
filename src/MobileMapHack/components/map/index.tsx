b78aa9073f814cd63206c26fd652ca871dd30ab1
kev <kev.hannen@gmail.com>
2019-06-16 09:00:12 +0100

    bloody key

--- src/MobileMapHack/components/map/index.tsx
+++ src/MobileMapHack/components/map/index.tsx
@@ -4,7 +4,7 @@ import {Text, View} from "react-native";
 import React from "react";
 import {Location} from "../../domain/types";
 
-const GOOGLE_MAPS_APIKEY = 'AIzaSyD3j6iuUnWDCgW6m6BmSsNjF-7FehkIS7o';
+const GOOGLE_MAPS_APIKEY = 'some key';
 
 export const  Map = (props) => {
     let origin:Location = props.navigation.getParam("origin");
