52af5c80c6d7ea7b7f99f6ff5465329269e3414a
kev <kev.hannen@gmail.com>
2019-06-16 08:59:32 +0100

    centre map based on route

--- src/MobileMapHack/components/map/index.tsx
+++ src/MobileMapHack/components/map/index.tsx
@@ -2,24 +2,30 @@ import MapView from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 import {Text, View} from "react-native";
 import React from "react";
+import {Location} from "../../domain/types";
 
-const origin = {latitude: 51.4511732, longitude: -0.2138706};
-const destination = {latitude: 51.5250836, longitude: -0.0769465};
-const GOOGLE_MAPS_APIKEY = 'Some key';
+const GOOGLE_MAPS_APIKEY = 'AIzaSyD3j6iuUnWDCgW6m6BmSsNjF-7FehkIS7o';
 
 export const  Map = (props) => {
+    let origin:Location = props.navigation.getParam("origin");
+    let dest:Location = props.navigation.getParam("destination");
+    let maxLoc =  {latitude: Math.max(origin.latitude, dest.latitude), longitude: Math.max(origin.longitude, dest.longitude)};
+    let minLoc =  {latitude: Math.min(origin.latitude, dest.latitude), longitude: Math.min(origin.longitude, dest.longitude)};
+    let latDelta = maxLoc.latitude - minLoc.latitude;
+    let lonDelta = maxLoc.longitude - minLoc.longitude;
+    let centre =  {latitude: minLoc.latitude + (0.5 * latDelta), longitude: minLoc.longitude + (0.5 * lonDelta)};
     return(<>
         <MapView
             style={{flex: 1}}
             initialRegion={{
-                latitude: 51.509864,
-                longitude: -0.118092,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+                latitude: centre.latitude,
+                longitude: centre.longitude,
+                latitudeDelta: 1.05 * latDelta,
+                longitudeDelta: 1.05 * lonDelta,
             }}>
             <MapViewDirections
-                origin={props.navigation.getParam("origin")}
-                destination={props.navigation.getParam("destination")}
+                origin={origin}
+                destination={dest}
                 apikey={GOOGLE_MAPS_APIKEY}
             />
         </MapView>
@@ -27,4 +33,4 @@ export const  Map = (props) => {
             <Text style={{flex: 0}}>Stuff</Text>
         </View>
         </>)
-}
+};
