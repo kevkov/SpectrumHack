86ad23b6df8e2bd42246a603d3cf187ac508dea1
Mohammad Rahman <MohammadRahman@tfl.gov.uk>
2019-06-18 11:20:20 +0100

    Initial checkin without any real implementaion.

---
+++ src/GoogleMapAPIWeb/Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GoogleMapAPIWeb.Models
+{
+    public class HomeViewModel
+    {
+
+        public HomeViewModel()
+        {
+            RouteInfos = new List<RouteInfo>();
+        }
+
+        public List<RouteInfo> RouteInfos { get; }
+    }
+
+}
---
+++ src/GoogleMapAPIWeb/Services/MapApiClient.cs
@@ -0,0 +1,21 @@
+﻿using GoogleMapAPIWeb.Models;
+using System;
+using System.Net.Http;
+
+namespace GoogleMapAPIWeb.Services
+{
+    public class MapApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public MapApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public HomeViewModel RouteInformation(int journeyId, bool showPollution, bool showSchools, TimeSpan startTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
