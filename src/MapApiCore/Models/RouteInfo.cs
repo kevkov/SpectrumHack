cd4b6fdfb352e22179bd67d93bbe812b198afb78
Mohammad Rahman <MohammadRahman@tfl.gov.uk>
2019-06-19 14:29:35 +0100

    Displaying (static) distance and duration

--- src/MapApiCore/Models/RouteInfo.cs
+++ src/MapApiCore/Models/RouteInfo.cs
@@ -7,8 +7,9 @@ namespace GoogleMapAPIWeb.Models
         public string ColorInHex { get; set; }
         public string RouteLabel { get; set; }
         public int PollutionPoint { get; set; }
-        public TimeSpan TravelTime { get; set; }
+        public string Duration { get; set; }
         public decimal TravelCost { get; set; }
         public int SchoolCount { get; set; }
+        public string Distance { get; set; }
     }
 }
