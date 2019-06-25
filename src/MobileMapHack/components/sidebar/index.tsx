88bf012d94529e3b8e74c7108d2015c1f0bdebe3
Kev Hannen <kev@stadion.io>
2019-06-25 17:49:24 +0100

    refactor for multiple item renderers

--- src/MobileMapHack/components/sidebar/index.tsx
+++ src/MobileMapHack/components/sidebar/index.tsx
@@ -15,22 +15,12 @@ import PersonImg from "../../assets/sadiqKhan.jpg";
 
 interface Section {
     title: string,
-    data: Journey[]
+    data: Journey[],
+    renderItem?: (data: any) => any;
 }
 
-const menuItems: Section[] = [
-    {title: 'Pay', data: []},
-    {title: 'Rewards', data: []},
-    {title: 'Badges', data: []},
-    {title: 'My Journeys', data: myJourneys},
-    {title: 'History', data: []},
-    {title: 'Help', data: []},
-    {title: 'Feedback', data: []},
-    {title: 'Logout', data: []},
-];
-
 function getRouteForSection(sectionTitle: string) {
-    switch(sectionTitle) {
+    switch (sectionTitle) {
         case 'Help':
             return 'Help';
         case 'Places':
@@ -42,78 +32,91 @@ function getRouteForSection(sectionTitle: string) {
 
 export const SideBar = (props: DrawerItemsProps) => {
 
-    const [openSections, setOpenSections] = useState<string[]>([]);
-
-    return (
-        <Container style={{paddingTop: Constants.statusBarHeight}}>
-            <Content>
-                <View style={{
-                    alignSelf: 'center',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                }}>
-                    <Image source={PersonImg} style={{width: 100, height: 100, borderRadius: 100, marginTop: 10}}/>
-                    <View style={{
-                        marginLeft: 15
-                    }}>
-                        <Text>Sadiq</Text>
-                        <Text>Khan</Text>
-                        <Badge success><Text>72</Text></Badge>
+        const [openSections, setOpenSections] = useState<string[]>([]);
+        const renderJourney = (data) => {
+            //console.log(JSON.stringify(data));
+            if (openSections.includes(data.section.title)) {
+                return (
+                    <View style={{marginLeft: 10, padding: 10, flexDirection: 'row'}}>
+                        <Icon
+                            onPress={() => {
+                                props.navigation.closeDrawer();
+                                props.navigation.navigate("Route",
+                                    {journey: data.item});
+                            }}
+                            active
+                            type={"MaterialIcons"}
+                            name={data.item.icon}
+                            style={{color: "#777", fontSize: 26, width: 30}}
+                        />
+                        <Text
+                            style={{paddingTop: 5}}
+                            onPress={() => {
+                                props.navigation.closeDrawer();
+                                props.navigation.navigate("Route",
+                                    {journey: data.item});
+                            }}>
+                            {data.item.name}
+                        </Text>
                     </View>
-                </View>
-                <SectionList style={{marginLeft: 15}}
-                             sections={menuItems}
-                             keyExtractor={((item, index) => item + index)}
-                             renderItem={(data) => {
-                                 //console.log(JSON.stringify(data));
-                                 if (openSections.includes(data.section.title)) {
-                                     return (
-                                         <View style={{marginLeft: 10, padding: 10, flexDirection: 'row'}}>
-                                             <Icon
-                                                 onPress={() => {
+                )
+            } else {
+                return null;
+            }
+        };
+
+        const menuItems: Section[] = [
+            {title: 'Pay', data: []},
+            {title: 'Rewards', data: []},
+            {title: 'Badges', data: []},
+            {title: 'My Journeys', data: myJourneys, renderItem: renderJourney},
+            {title: 'History', data: []},
+            {title: 'Help', data: []},
+            {title: 'Feedback', data: []},
+            {title: 'Logout', data: []},
+        ];
+
+        return (
+            <Container style={{paddingTop: Constants.statusBarHeight}}>
+                <Content>
+                    <View style={{
+                        alignSelf: 'center',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}>
+                        <Image source={PersonImg} style={{width: 100, height: 100, borderRadius: 100, marginTop: 10}}/>
+                        <View style={{
+                            marginLeft: 15
+                        }}>
+                            <Text>Sadiq</Text>
+                            <Text>Khan</Text>
+                            <Badge success><Text>72</Text></Badge>
+                        </View>
+                    </View>
+                    <SectionList style={{marginLeft: 15}}
+                                 sections={menuItems}
+                                 keyExtractor={((item, index) => item + index)}
+                                 renderSectionHeader=
+                                     {(data) => (
+                                         <Text
+                                             style={{padding: 10, fontWeight: 'bold'}}
+                                             onPress={() => {
+                                                 if (data.section.data) {
+                                                     if (openSections.includes(data.section.title)) {
+                                                         setOpenSections(openSections.filter((s) => s !== data.section.title));
+                                                     } else {
+                                                         setOpenSections(openSections.concat(data.section.title));
+                                                     }
+                                                 } else {
                                                      props.navigation.closeDrawer();
-                                                     props.navigation.navigate("Route",
-                                                         {journey: data.item});
-                                                 }}
-                                                 active
-                                                 type={"MaterialIcons"}
-                                                 name={data.item.icon}
-                                                 style={{color: "#777", fontSize: 26, width: 30}}
-                                             />
-                                             <Text
-                                                 style={{paddingTop: 5}}
-                                                 onPress={() => {
-                                                     props.navigation.closeDrawer();
-                                                     props.navigation.navigate("Route",
-                                                         {journey: data.item});
-                                                 }}>
-                                                 {data.item.name}
-                                             </Text>
-                                         </View>
-                                     )
-                                 } else {
-                                     return null;
-                                 }}}
-                             renderSectionHeader=
-                                 {(data) => (
-                                     <Text
-                                        style={{padding: 10, fontWeight: 'bold'}}
-                                        onPress={() => {
-                                            if (data.section.data) {
-                                                if (openSections.includes(data.section.title)) {
-                                                    setOpenSections(openSections.filter((s) => s !== data.section.title));
-                                                } else {
-                                                    setOpenSections(openSections.concat(data.section.title));
-                                                }
-                                            } else {
-                                                props.navigation.closeDrawer();
-                                                props.navigation.navigate(getRouteForSection(data.section.title));
-                                            }
-                                        }}>
-                                     {data.section.title}
-                                     </Text>)}
-                />
-            </Content>
-        </Container>
-    )
-};
+                                                     props.navigation.navigate(getRouteForSection(data.section.title));
+                                                 }
+                                             }}>
+                                             {data.section.title}
+                                         </Text>)}
+                    />
+                </Content>
+            </Container>
+        )
+    }
+;
